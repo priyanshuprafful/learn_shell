97c06dc47c5faf8648ee4e0d2999dfa932dc1c57
priyanshuprafful <priyanshuprafful@gmail.com>
2023-08-29 18:49:25 +0530

    updating ./learn_shell

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,3 +1,9 @@
-echo $1
-echo $2
-echo $3 
+echo "First Argument $1" # first argument
+echo "Second Argument $2" # second argument
+echo "Script Name $0" # script name
+echo "All arguments $*" # All arguments
+echo "All arguments $@ "  # All arguments
+echo "Number of arguments = $# " # Number of Arguments
+# Reserved Variables
+# 0 - n , 1 , 2 ,3 are first , second , third arguments
+# $0 is the script name
