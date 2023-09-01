433408fed445262980373078550ca5fadc9262ac
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-01 16:05:53 +0530

    updating ./learn_shell

--- 05-functions.sh
+++ 05-functions.sh
@@ -6,8 +6,8 @@ xyz() {
   echo "Second Arguments ${1}"
   echo "All Arguments ${*}"
   echo "No. Of arguments ${#}"
-  echo "No. Of arguments ${$#}"
-  echo "No. Of arguments {$#}"
+  echo "No. Of arguments ${$#}" # garbage value dega
+  echo "No. Of arguments {$#}"  # ye {} ko as a string ka part samjhega
 }
 
 xyz 123 312
