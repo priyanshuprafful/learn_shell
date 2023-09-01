c783f2cb092d0f09ecab4947a670da4e0903fe8d
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-01 16:02:56 +0530

    updating ./learn_shell

--- 05-functions.sh
+++ 05-functions.sh
@@ -2,6 +2,10 @@
 
 xyz() {
   echo "Hello From Function"
+  echo "First Arguments ${1}"
+  echo "Second Arguments ${1}"
+  echo "All Arguments ${*}"
+  echo "No. Of arguments ${#}"
 }
 
-xyz
+xyz 123 312
