d6508ef5f6b26fc15674191de78a70f35c18f439
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-02 14:40:46 +0530

    updating ./learn_shell

--- 06-condtionals.sh
+++ 06-condtionals.sh
@@ -1,10 +1,12 @@
 number=$1
 string=$2
 
-if [ ${number} -eq 5 ]; then # then can go in next line or else use ; and THEN write then
+if [ "${number}" -eq 5 ]; then # then can go in next line or else use ; and THEN write then
   echo  "number is ${number}"
 fi
 
-if [ ${string} -eq "abc" ]; then
+if [ "${string}" -eq "abc" ]; then
     echo "String is ${string}"
 fi
+
+# it is always a good practice to quote the variable in the expressions
