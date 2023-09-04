0d532015a234963e06b4fd303db373c27581fc85
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-04 15:32:11 +0530

    updating ./learn_shell

--- 09-loops.sh
+++ 09-loops.sh
@@ -7,4 +7,10 @@ while [ $i -gt 0 ]; do
   echo $i # we have to use a condition otherwise it is going to be a infinite loop
   i=$(($i-1))  # this is arithemetic substitution we have in variables
   sleep 1 # delay 1 second in between
-done 
+done
+
+# 2. loops based on inputs
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ; do
+  echo $i
+  sleep 1
+done
