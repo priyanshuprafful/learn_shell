a3b9f9e20b62480d16e6a2cfb178e17525154d9a
priyanshuprafful <priyanshuprafful@gmail.com>
2023-09-04 14:45:22 +0530

    updating ./learn_shell

--- 08-case-condition.sh
+++ 08-case-condition.sh
@@ -18,3 +18,14 @@ case ${name} in
   *) echo "No course Available"
     ;;
 esac
+
+# same code in if condition
+if [ ${name} == "DevOps" ]; then
+  echo "Welcome to Devops training "
+  echo "Timings are 6AM"
+elif [ ${name} == "AWS" ]; then
+  echo "Welcome to AWS training "
+  echo "Timings are 7AM"
+else
+  echo "No course Available"
+fi
